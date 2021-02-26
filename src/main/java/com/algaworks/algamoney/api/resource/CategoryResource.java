adca0541f9375f85b6df11e4d3321175367109af
Bruno Dantas <brunoodantas7@gmail.com>
2021-02-25 21:01:38 -0300

    exemplo tratamento de headers de retorno com codigo http referentes usando ResponseEntity

--- src/main/java/com/algaworks/algamoney/api/resource/CategoryResource.java
+++ src/main/java/com/algaworks/algamoney/api/resource/CategoryResource.java
@@ -4,6 +4,7 @@ import com.algaworks.algamoney.api.model.Category;
 import com.algaworks.algamoney.api.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,4 +24,9 @@ public class CategoryResource {
     public List<Category> listAll(){
         return categoryRepository.findAll();
     }
+   /* @GetMapping
+    public ResponseEntity<?> listAll(){ //USANDO REPONSE ENITTY PARA TRATAMETNO DO JSON DE RETORNO COM CODIGOS HTTP
+        List<Category> categories = categoryRepository.findAll();
+        return !categories.isEmpty() ? ResponseEntity.ok(categories) : ResponseEntity.noContent().build();
+    }*/
 }
