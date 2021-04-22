5208a1b02b31b00f9055b24625494d9a3540255b
Bruno Dantas <brunoodantas7@gmail.com>
2021-04-21 23:12:17 -0300

    mapeamento model e DLL conlucido , pendente finalizar implementacao de persistir o usuarios da api no banco

---
+++ src/main/java/com/algaworks/algamoney/api/model/Permission.java
@@ -0,0 +1,46 @@
+package com.algaworks.algamoney.api.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "permission")
+public class Permission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String description;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Permission that = (Permission) o;
+        return Objects.equals(id, that.id) && Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, description);
+    }
+}
---
+++ src/main/resources/db/migration/V04__create_and_register_users_and_permissions.sql
@@ -0,0 +1,50 @@
+create table user(
+    id serial primary key ,
+    name varchar(50) not null ,
+    email varchar(50) not null,
+    password varchar(150) not null
+);
+
+create table permission(
+    id serial primary key ,
+    description varchar(50) not null
+);
+
+create table user_permission(
+--     id serial primary key,
+    id_user integer not null ,
+    id_permission integer not null,
+    primary key (id_user,id_permission),
+    foreign key (id_user) references user(id),
+    foreign key (id_permission) references permission(id)
+);
+
+--INSERTS CARGA
+INSERT INTO user (id, name, email, password) values (1, 'Administrador', 'admin@algamoney.com', '$2a$10$X607ZPhQ4EgGNaYKt3n4SONjIv9zc.VMWdEuhCuba7oLAL5IvcL5.');
+INSERT INTO user (id, name, email, password) values (2, 'Maria Silva', 'maria@algamoney.com', '$2a$10$Zc3w6HyuPOPXamaMhh.PQOXvDnEsadztbfi6/RyZWJDzimE8WQjaq');
+
+INSERT INTO permission (id, description) values (1, 'ROLE_CADASTRAR_CATEGORIA');
+INSERT INTO permission (id, description) values (2, 'ROLE_PESQUISAR_CATEGORIA');
+
+INSERT INTO permission (id, description) values (3, 'ROLE_CADASTRAR_PESSOA');
+INSERT INTO permission (id, description) values (4, 'ROLE_REMOVER_PESSOA');
+INSERT INTO permission (id, description) values (5, 'ROLE_PESQUISAR_PESSOA');
+
+INSERT INTO permission (id, description) values (6, 'ROLE_CADASTRAR_LANCAMENTO');
+INSERT INTO permission (id, description) values (7, 'ROLE_REMOVER_LANCAMENTO');
+INSERT INTO permission (id, description) values (8, 'ROLE_PESQUISAR_LANCAMENTO');
+
+-- admin
+INSERT INTO user_permission (id_user, id_permission) values (1, 1);
+INSERT INTO user_permission (id_user, id_permission) values (1, 2);
+INSERT INTO user_permission (id_user, id_permission) values (1, 3);
+INSERT INTO user_permission (id_user, id_permission) values (1, 4);
+INSERT INTO user_permission (id_user, id_permission) values (1, 5);
+INSERT INTO user_permission (id_user, id_permission) values (1, 6);
+INSERT INTO user_permission (id_user, id_permission) values (1, 7);
+INSERT INTO user_permission (id_user, id_permission) values (1, 8);
+
+-- maria
+INSERT INTO user_permission (id_user, id_permission) values (2, 2);
+INSERT INTO user_permission (id_user, id_permission) values (2, 5);
+INSERT INTO user_permission (id_user, id_permission) values (2, 8);
