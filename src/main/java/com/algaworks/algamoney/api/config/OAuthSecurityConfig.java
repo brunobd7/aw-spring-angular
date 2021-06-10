69fe3cd0b6be526d16c198ed1a355eb1e394b63a
Bruno Dantas <brunoodantas7@gmail.com>
2021-06-10 14:59:32 -0300

    Adicionado config de PROFILES SPRING para gerenciar ambientes em niveis de segurança diferentes, validar se ResourceServerConfig pnecessita da anotacaoe testar a API

--- src/main/java/com/algaworks/algamoney/api/config/OAuthSecurityConfig.java
+++ src/main/java/com/algaworks/algamoney/api/config/OAuthSecurityConfig.java
@@ -2,6 +2,7 @@ package com.algaworks.algamoney.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  * Com as atualizações do Spring Security, não há um Bean para AuthenticationManager que é fornecido por padrão pelo Spring,
  * para isso precisamos definir esse Bean por conta própria.
  * */
+@Profile("oauth2-security")
 @Configuration
 @EnableWebSecurity
 public class OAuthSecurityConfig extends WebSecurityConfigurerAdapter {
